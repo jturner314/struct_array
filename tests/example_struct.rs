8e19bc7263ba5cafbe32919d75ccf08573952788
Jim Turner <jturner314@gmail.com>
2017-01-04 21:53:17 -0500

    Clean up tests

--- tests/example_struct.rs
+++ tests/example_struct.rs
@@ -15,185 +15,182 @@ struct Example {
 
 #[test]
 fn test_deref() {
-    use std::ops::Deref;
-
-    let foo = Example { x: 0, y: 1 };
-    let bar: &[u32; 2] = &foo.deref();
-    assert_eq!(bar, &[0, 1]);
+    let example = Example { x: 0, y: 1 };
+    assert_eq!(*example, [0, 1]);
 }
 
 #[test]
 fn test_deref_mut() {
     use std::ops::DerefMut;
 
-    let mut foo = Example { x: 0, y: 1 };
+    let mut example = Example { x: 0, y: 1 };
     {
-        let bar: &mut [u32; 2] = &mut foo.deref_mut();
-        bar[1] = 2;
-        assert_eq!(bar, &[0, 2]);
+        let array: &mut [u32; 2] = &mut example.deref_mut();
+        array[1] = 2;
+        assert_eq!(array, &[0, 2]);
     }
-    assert_eq!(foo, Example { x: 0, y: 2 });
+    assert_eq!(example, Example { x: 0, y: 2 });
 }
 
 #[test]
 fn test_into_array() {
-    let foo = Example { x: 0, y: 1 };
-    let bar: [u32; 2] = foo.into();
-    assert_eq!(bar, [0, 1]);
+    let example = Example { x: 0, y: 1 };
+    let array: [u32; 2] = example.into();
+    assert_eq!(array, [0, 1]);
 }
 
 #[test]
 fn test_from_array() {
-    let foo = [0, 1];
-    let mut bar: Example = foo.into();
-    bar.y = 2;
-    assert_eq!(bar, Example { x: 0, y: 2 });
+    let array = [0, 1];
+    let mut example: Example = array.into();
+    example.y = 2;
+    assert_eq!(example, Example { x: 0, y: 2 });
 }
 
 #[test]
 fn test_into_array_ref() {
-    let foo = &Example { x: 0, y: 1 };
-    let bar: &[u32; 2] = foo.into();
-    assert_eq!(bar, &[0, 1]);
+    let example = &Example { x: 0, y: 1 };
+    let array: &[u32; 2] = example.into();
+    assert_eq!(array, &[0, 1]);
 }
 
 #[test]
 fn test_struct_ref_as_array_ref() {
-    let foo = &Example { x: 0, y: 1 };
-    let bar: &[u32; 2] = foo.as_ref();
-    assert_eq!(bar, &[0, 1]);
+    let example = &Example { x: 0, y: 1 };
+    let array: &[u32; 2] = example.as_ref();
+    assert_eq!(array, &[0, 1]);
 }
 
 #[test]
 fn test_from_array_ref() {
-    let foo = &[0, 1];
-    let bar: &Example = foo.into();
-    assert_eq!(bar, &Example { x: 0, y: 1 });
+    let array = &[0, 1];
+    let example: &Example = array.into();
+    assert_eq!(example, &Example { x: 0, y: 1 });
 }
 
 #[test]
 fn test_array_ref_as_struct_ref() {
-    let foo = &[0, 1];
-    let bar: &Example = foo.as_ref();
-    assert_eq!(bar, &Example { x: 0, y: 1 });
+    let array = &[0, 1];
+    let example: &Example = array.as_ref();
+    assert_eq!(example, &Example { x: 0, y: 1 });
 }
 
 #[test]
 fn test_into_array_ref_mut() {
-    let mut foo = &mut Example { x: 0, y: 1 };
+    let mut example = &mut Example { x: 0, y: 1 };
     {
-        let mut bar: &mut [u32; 2] = foo.into();
-        bar[1] = 2;
-        assert_eq!(bar, &mut [0, 2]);
+        let mut array: &mut [u32; 2] = example.into();
+        array[1] = 2;
+        assert_eq!(array, &mut [0, 2]);
     }
-    assert_eq!(foo, &mut Example { x: 0, y: 2 });
+    assert_eq!(example, &mut Example { x: 0, y: 2 });
 }
 
 #[test]
 fn test_struct_ref_mut_as_array_ref_mut() {
-    let mut foo = &mut Example { x: 0, y: 1 };
+    let mut example = &mut Example { x: 0, y: 1 };
     {
-        let mut bar: &mut [u32; 2] = foo.as_mut();
-        bar[1] = 2;
-        assert_eq!(bar, &mut [0, 2]);
+        let mut array: &mut [u32; 2] = example.as_mut();
+        array[1] = 2;
+        assert_eq!(array, &mut [0, 2]);
     }
-    assert_eq!(foo, &mut Example { x: 0, y: 2 });
+    assert_eq!(example, &mut Example { x: 0, y: 2 });
 }
 
 #[test]
 fn test_from_array_ref_mut() {
-    let foo = &mut [0, 1];
+    let array = &mut [0, 1];
     {
-        let mut bar: &mut Example = foo.into();
-        bar.y = 2;
-        assert_eq!(bar, &mut Example { x: 0, y: 2 });
+        let mut example: &mut Example = array.into();
+        example.y = 2;
+        assert_eq!(example, &mut Example { x: 0, y: 2 });
     }
-    assert_eq!(foo, &mut [0, 2]);
+    assert_eq!(array, &mut [0, 2]);
 }
 
 #[test]
 fn test_array_ref_mut_as_struct_ref_mut() {
-    let foo = &mut [0, 1];
+    let array = &mut [0, 1];
     {
-        let mut bar: &mut Example = foo.as_mut();
-        bar.y = 2;
-        assert_eq!(bar, &mut Example { x: 0, y: 2 });
+        let mut example: &mut Example = array.as_mut();
+        example.y = 2;
+        assert_eq!(example, &mut Example { x: 0, y: 2 });
     }
-    assert_eq!(foo, &mut [0, 2]);
+    assert_eq!(array, &mut [0, 2]);
 }
 
 #[test]
 fn test_into_slice_ref() {
-    let foo = Example { x: 0, y: 1 };
-    let bar: &[u32] = (&foo).into();
-    assert_eq!(bar, [0, 1]);
+    let example = Example { x: 0, y: 1 };
+    let slice: &[u32] = (&example).into();
+    assert_eq!(slice, [0, 1]);
 }
 
 #[test]
 fn test_array_ref_as_slice_ref() {
-    let foo = Example { x: 0, y: 1 };
-    let bar: &[u32] = (&foo).as_ref();
-    assert_eq!(bar, [0, 1]);
+    let example = Example { x: 0, y: 1 };
+    let slice: &[u32] = (&example).as_ref();
+    assert_eq!(slice, [0, 1]);
 }
 
 #[test]
 fn test_from_slice_ref() {
-    let foo = [0, 1];
-    let bar: &[u32] = &foo;
-    let baz: &Example = bar.into();
-    assert_eq!(baz, &Example { x: 0, y: 1 });
+    let array = [0, 1];
+    let slice: &[u32] = &array;
+    let example: &Example = slice.into();
+    assert_eq!(example, &Example { x: 0, y: 1 });
 }
 
 #[test]
 fn test_slice_ref_as_array_ref() {
-    let foo = [0, 1];
-    let bar: &[u32] = &foo;
-    let baz: &Example = bar.as_ref();
-    assert_eq!(baz, &Example { x: 0, y: 1 });
+    let array = [0, 1];
+    let slice: &[u32] = &array;
+    let example: &Example = slice.as_ref();
+    assert_eq!(example, &Example { x: 0, y: 1 });
 }
 
 #[test]
 fn test_into_slice_ref_mut() {
-    let mut foo = Example { x: 0, y: 1 };
+    let mut example = Example { x: 0, y: 1 };
     {
-        let bar: &mut [u32] = (&mut foo).into();
-        bar[1] = 2;
-        assert_eq!(bar, &mut [0, 2]);
+        let slice: &mut [u32] = (&mut example).into();
+        slice[1] = 2;
+        assert_eq!(slice, &mut [0, 2]);
     }
-    assert_eq!(foo, Example { x: 0, y: 2 });
+    assert_eq!(example, Example { x: 0, y: 2 });
 }
 
 #[test]
 fn test_struct_ref_mut_as_slice_ref_mut() {
-    let mut foo = Example { x: 0, y: 1 };
+    let mut example = Example { x: 0, y: 1 };
     {
-        let bar: &mut [u32] = (&mut foo).as_mut();
-        bar[1] = 2;
-        assert_eq!(bar, &mut [0, 2]);
+        let slice: &mut [u32] = (&mut example).as_mut();
+        slice[1] = 2;
+        assert_eq!(slice, &mut [0, 2]);
     }
-    assert_eq!(foo, Example { x: 0, y: 2 });
+    assert_eq!(example, Example { x: 0, y: 2 });
 }
 
 #[test]
 fn test_from_slice_ref_mut() {
-    let mut foo = [0, 1];
+    let mut array = [0, 1];
     {
-        let mut bar: &mut [u32] = &mut foo;
-        let mut baz: &mut Example = bar.into();
-        baz.y = 2;
-        assert_eq!(baz, &Example { x: 0, y: 2 });
+        let mut slice: &mut [u32] = &mut array;
+        let mut example: &mut Example = slice.into();
+        example.y = 2;
+        assert_eq!(example, &Example { x: 0, y: 2 });
     }
-    assert_eq!(foo, [0, 2]);
+    assert_eq!(array, [0, 2]);
 }
 
 #[test]
 fn test_ref_mut_slice_as_struct_ref_mut() {
-    let mut foo = [0, 1];
+    let mut array = [0, 1];
     {
-        let mut bar: &mut [u32] = &mut foo;
-        let mut baz: &mut Example = bar.as_mut();
-        baz.y = 2;
-        assert_eq!(baz, &Example { x: 0, y: 2 });
+        let mut slice: &mut [u32] = &mut array;
+        let mut example: &mut Example = slice.as_mut();
+        example.y = 2;
+        assert_eq!(example, &Example { x: 0, y: 2 });
     }
-    assert_eq!(foo, [0, 2]);
+    assert_eq!(array, [0, 2]);
 }
